c2497d1aff0f28e1c8d98c0522bc44793eed40c4
Randy Truong <rtruo001@ucr.edu>
2015-03-13 00:51:13 -0700

    Reverted commits, fixed background scaling, lost video, more updates

---
+++ 2MyWebpage/js/website.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("body").hide().fadeIn(1500);
+  $("#pictureOfMe").hide().fadeIn(6000);
+});
+
+$(document).ready(function(){
+  $('#nav').localScroll(800);
+  // $('#home').parallax("50%", 0.1);
+  // $('#aboutMe').parallax("50%", 0.1);
+  // $('#resume').parallax("50%", 0.1);
+  // $('#myLinks').parallax("50%", 0.1);
+})
+
+
